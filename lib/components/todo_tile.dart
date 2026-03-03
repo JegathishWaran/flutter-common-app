5bea51c50c43bde82d9a311c8a09358eec29d8c3
StaanERP <jegathish.e@staan.in>
2026-03-04 04:04:07 +0530

    create list view

---
+++ lib/components/todo_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TodoTile extends StatelessWidget {
+  String taskName;
+  bool taskCompleted;
+  Function(bool?)? onChange;
+
+  TodoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChange,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChange),
+
+            Text(taskName),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:comman_app/components/todo_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,13 +11,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.yellow[200],
-      appBar:  AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.yellow,
         title: Text("TO DO"),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          TodoTile(
+            taskName: "Make it today",
+            taskCompleted: false,
+            onChange: (pk) {},
+          ),
+        ],
       ),
     );
   }
